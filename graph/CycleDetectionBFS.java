36a39c2880b7fb78e94c11b333d0974d3fbc8a26
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-03-05 03:07:54 +0600

    add bfs and dfs and cycal detected in bfs

---
+++ graph/CycleDetectionBFS.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+public class CycleDetectionBFS {
+    static class Edge {
+        int src, dest;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[1].add(new Edge(1, 0));
+
+        graph[1].add(new Edge(1, 2));
+        graph[2].add(new Edge(2, 1));
+
+        graph[2].add(new Edge(2, 3));
+        graph[3].add(new Edge(3, 2));
+
+        graph[3].add(new Edge(3, 4));
+        graph[4].add(new Edge(4, 3));
+
+        graph[4].add(new Edge(4, 1));
+        graph[1].add(new Edge(1, 4));
+    }
+
+    static boolean isCyclicBFS(ArrayList<Edge>[] graph, int V) {
+        boolean[] visited = new boolean[V];
+        int[] parent = new int[V];
+        Arrays.fill(parent, -1);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (bfsDetectCycle(graph, visited, parent, i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean bfsDetectCycle(ArrayList<Edge>[] graph, boolean[] visited, int[] parent, int src) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+        visited[src] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (Edge edge : graph[node]) {
+                int neighbor = edge.dest;
+
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    parent[neighbor] = node;
+                    queue.add(neighbor);
+                } else if (neighbor != parent[node]) {
+                    System.out.print("Cycle detected: ");
+                    printCyclePath(parent, node, neighbor);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static void printCyclePath(int[] parent, int start, int end) {
+        List<Integer> cyclePath = new ArrayList<>();
+        int node = start;
+
+        while (node != -1) {
+            cyclePath.add(node);
+            if (node == end) break;
+            node = parent[node];
+        }
+        Collections.reverse(cyclePath);
+        cyclePath.add(end);
+
+        for (int i = 0; i < cyclePath.size(); i++) {
+            System.out.print(cyclePath.get(i));
+            if (i < cyclePath.size() - 1) System.out.print(" -> ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        if (!isCyclicBFS(graph, V)) {
+            System.out.println("No cycle found in the graph.");
+        }
+    }
+}
--- graph/dfs.java
+++ graph/dfs.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BFS {
+public class dfs {
     static class Edge {
         int src;
         int dest;
@@ -22,11 +22,13 @@ public class BFS {
         graph[0].add(new Edge(0, 2, 1));
         graph[1].add(new Edge(1, 0, 1));
         graph[1].add(new Edge(1, 3, 1));
+
         graph[2].add(new Edge(2, 0, 1));
         graph[2].add(new Edge(2, 4, 1));
         graph[3].add(new Edge(3, 1, 1));
         graph[3].add(new Edge(3, 4, 1));
         graph[3].add(new Edge(3, 5, 1));
+
         graph[4].add(new Edge(4, 2, 1));
         graph[4].add(new Edge(4, 3, 1));
         graph[4].add(new Edge(4, 5, 1));
@@ -36,37 +38,32 @@ public class BFS {
         graph[6].add(new Edge(6, 5, 1));
     }
 
-    public static void bfs(ArrayList<Edge> graph[], int V) {
-        boolean visited[] = new boolean[V];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0); // Source = 0
-
-        while (!q.isEmpty()) {
-            int curr = q.remove();
-            if (!visited[curr]) {
-                System.out.print(curr + " ");
-                visited[curr] = true;
-               for (int i = 0; i < graph[curr].size(); i++) {
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
-            }
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean visited[]) {
+        if (visited[curr]) {
+            return;
         }
-        System.out.println();
+
+        System.out.print(curr + " ");
+        visited[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            dfs(graph, e.dest, visited);
+        }
     }
 
     public static void main(String args[]) {
         /*
               1 --- 3
-             / |   \
-            0  |    5 -- 6
-             \ |   /
+             / |    \
+            0  |     5 -- 6
+             \ |    /
               2 -- 4
         */
-
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
+
         createGraph(graph);
-        bfs(graph, V);
+        dfs(graph, 0, new boolean[V]);
     }
 }
